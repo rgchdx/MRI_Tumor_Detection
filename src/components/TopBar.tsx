de836c1ce4a89192bea15a7a6bc03e5de77c7d59
Richie Dix <rgchdx@gmail.com>
2025-03-27 15:57:36 -0500

    MRI BONANZA

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TopBar = () => {
     return(
         <div className="flex items-center justify-between bg-gray-800 p-4 text-white">
-            <h1 className="text-lg font-bold">My Application</h1>
+            <h1 className="text-lg font-bold">MRI Bonanza</h1>
             <div className="flex space-x-4">
                 <a href="#" className="hover:text-gray-400">Home</a>
                 <a href="#" className="hover:text-gray-400">About</a>
