8b64cbbcfe51c54c3891e812204ed9589d94f5c1
Richie Dix <rgchdx@gmail.com>
2025-04-06 00:14:58 -0500

    Added packages and bottombar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TopBar from './components/TopBar'
 import Hero from './components/Hero'
