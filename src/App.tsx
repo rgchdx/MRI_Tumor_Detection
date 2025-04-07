4b669de60969a892fd9ca43d83824e59f8423cac
Richie Dix <rgchdx@gmail.com>
2025-04-06 19:20:05 -0500

    Deleted unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import TopBar from './components/TopBar'
 import Hero from './components/Hero'
 import BottomBar from './components/BottomBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
