6b63067c0ebea0372e98531e1d9188aa003681f0
Richie Dix <rgchdx@gmail.com>
2025-05-01 15:18:27 -0500

    Reading in the pickle file

--- server/torch/main.py
+++ server/torch/main.py
@@ -3,27 +3,59 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse, StreamingResponse
 import os
 import pickle
+from torchvision import transforms
+import torch
+from torch import nn
+import timm
+import __main__
+
+
+
+user_dir = os.getcwd()
+file = 'model.pkl'
+model_path = os.path.join(user_dir, file)
+
+class MRIClassifer(nn.Module):
+    def __init__(self, num_classes=53):
+        super(MRIClassifer, self).__init__()
+        # Where we define all the parts of the model
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+        # Make a classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+    
+    def forward(self, x):
+        # Connect these parts and return the output
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
 
 # Load the picke file
-with open("model.pkl", "rb") as file:
-    model = pickle.load(file)
+model = MRIClassifer()  # Initialize model structure
+model.load_state_dict(torch.load(model_path, map_location='torch.device("cpu")'))
+model.eval()
 
-# should we move this to a completely different file that would unpack the model???
-class MRIClassifier:
-    def __init__(self, model):
-        self.model = model
-    
-    ## change this so that it will generate what we need
-    def predict(self, image_path: str):
-        # Here you would typically process the image and make a prediction
-        # For simplicity, let's assume the model has a predict method
-        return self.model.predict(image_path)
-    
-    def __main__(self):
-        # This method could be used to test the model locally
-        pass
-
+# Initialize the FastAPI app
 app = FastAPI()
+
+# Initialize the MRIClassifier with the loaded model
+classifier = MRIClassifer(model)
+
+# Function to preprocess the image to match the model's input requirements
+def preprocess(image):
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.PILToTensor(),
+    ])
+    return transform(image)
+
+# Set the device to CPU (if we are running on GPU then change this)
+device = torch.device('cpu')
 
 # Endpoint to serve the HTML page
 @app.get("/")
