63473e7bd6f4e5bdaf4d12a16cc59d4e54c684e3
Richie Dix <rgchdx@gmail.com>
2025-05-03 21:55:16 -0500

    Configured so that predict returns something

--- server/torch/main.py
+++ server/torch/main.py
@@ -8,6 +8,7 @@ import torch
 from MRIClassifer import MRIClassifer
 import __main__
 import torch.serialization
+from PIL import Image
 
 __main__.MRIClassifer = MRIClassifer
 
@@ -46,16 +47,30 @@ async def root():
     return {"message": "Welcome to MRI Bonanza"}
 
 ### Endpoint to upload an image and get a prediction
+# Later on we will fetch the data from the database and get the prediction results into the frontend
+# Might have to change so that we will take in the image from the frontend and then return the prediction results
+# For testing we have it set so that we have a test image in the server/torch directory might have to change the input later 
 @app.post("/predict")
 async def predict(files: List[UploadFile] = File(...)):
     if not files:
         return {"error": "No files uploaded"}
-    if files[0].split(".")[-1] != "jpg" or files[0].split(".")[-1] != "png":
-        return {"error": "Only jpg and png files are allowed"}
+    # Check if the uploaded file is an image
+    if not files[0].filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        return {"error": "File must be an image (png, jpg, jpeg)"}
     # save the uploaded file
-    file_locaiton = f"server/torch/{files[0].filename}"
-    with open(file_locaiton, "wb") as f:
+    # Maybe change the file location to a more appropriate place
+    #file_locaiton = f"../server/torch/{files[0].filename}"
+    file_location = f"server/torch/no_tumor_test1.png"
+    os.makedirs(os.path.dirname(file_location), exist_ok=True)
+    print(f"Saving file to {file_location}")
+    with open(file_location, "wb") as f:
         f.write(await files[0].read())
+    image = Image.open(file_location).convert("RGB")
+    input_tensor = preprocess(image).unsqueeze(0).float()
+    print(f"Input tensor shape: {input_tensor.shape}")
+    with torch.no_grad():
+        output = model(input_tensor)
+    print(f"Output shape: {output.shape}")
     # Make prediction
-    prediction = model.predict(file_locaiton)
+    prediction = torch.argmax(output, dim=1).item()
     return {"prediction": prediction}
--- server/torch/requesttest.py
+++ server/torch/requesttest.py
@@ -1,7 +1,7 @@
 import pip._vendor.requests 
 
 url = "http://127.0.0.1:8000/predict"
-file_path = "/Users/rgdix/Desktop/MRI_Tumor_Detection/server/torch/test/has_tumor_test1.png" # add some image here or add a test file for tumor images
+file_path = "test/no_tumor_test1.png" # add some image here or add a test file for tumor images
 
 with open(file_path, "rb") as file:
     files = {"files": (file_path, file, "image/jpeg")}
